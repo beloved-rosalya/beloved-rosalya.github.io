72abc121fc8c9d688ed18acb291d7d89b0ee888b
cudeapito9000 <158750845+cudeapito9000@users.noreply.github.com>
2024-02-03 20:17:06 -0300

    update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,5 +1,2 @@
-<script type="module">
-  import { fairyDustCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
-
-  new fairyDustCursor();
-</script>
+import { emojiCursor } from "cursor-effects";
+new emojiCursor({ emoji: ["🔥", "🐬", "🦆"] });
