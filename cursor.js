1ba3cdd01c068ba668047a8d0f119e76b2377331
cudeapito9000 <158750845+cudeapito9000@users.noreply.github.com>
2024-02-03 20:12:04 -0300

    update cursor.js and index.html

--- cursor.js
+++ cursor.js
@@ -1,1 +1,5 @@
-<script src="https://unpkg.com/cursor-effects@latest/dist/browser.js"></script>
+<script type="module">
+  import { fairyDustCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
+
+  new fairyDustCursor();
+</script>
